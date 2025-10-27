c837a05795b645cfc963d91ee3ce355d13a122cd
Utsav Raj <utsavraj2004u@gmail.com>
2025-10-27 09:16:21 +0530

    Fix: Firebase Vite build issue
    :wq

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      external: ['firebase', 'firebase/app', 'firebase/auth', 'firebase/firestore', 'firebase/storage'],
+    },
+  },
+  optimizeDeps: {
+    include: ['firebase/app', 'firebase/auth', 'firebase/firestore', 'firebase/storage'],
+  },
+  experimental: {
+    useRollup: true, // force disable rolldown
+  },
 })
